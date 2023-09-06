6dc118c1c14c45cdf9bb406904a545f5da372e66
Jérémy Dillenbourg <jeremy@dillenbourg.be>
2023-09-06 12:36:59 +0200

    overview layout

--- module/Reporting/Http/Console/YearlyOverview.php
+++ module/Reporting/Http/Console/YearlyOverview.php
@@ -6,6 +6,9 @@ use Illuminate\Console\Command;
 use Module\Reporting\Application\GetYearlyOverviewQuery;
 use Module\SharedKernel\Domain\Bus;
 use Module\SharedKernel\Domain\ClockInterface;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use function Laravel\Prompts\text;
 
 class YearlyOverview extends Command
 {
@@ -15,15 +18,34 @@ class YearlyOverview extends Command
 
     public function handle(Bus $bus, ClockInterface $clock): int
     {
-        $year = $this->ask('Which year result do you want to print?', $clock->now()->year);
+        $year = text('Which year result do you want to print?', $clock->now()->year, $clock->now()->year);
 
         $query = new GetYearlyOverviewQuery($year);
         $result = $bus->dispatch($query);
 
 
+        $table = new Table($this->output);
+        $separator = new TableSeparator;
 
-        dd($result);
+        $table->setHeaderTitle('Results for year ' . $year);
+        $table->setHorizontal();
+        $table->setHeaders(['Income', 'Expense', 'Inc. deductible', 'Net taxable', 'Social contribution', 'Taxable', 'Tax', 'Vat to pay', 'Vat to get back']);
 
+        $table->setRows([
+            [
+                $result['bill']['total'] .' €',
+                $result['expense']['totalExpense'] .' €',
+                $result['expense']['totalDeductibleExpense'] .' €',
+                $result['bill']['total'] - $result['expense']['totalDeductibleExpense'] .' €',
+                $result['socialContribution']['yearly_amount'] .' €',
+                $result['taxable_income'] .' €',
+                '<error>' . $result['tax'] . ' €</error>',
+                $result['bill']['totalVatCollected'] .' €',
+                $result['expense']['vatToRecover'] .' €',
+            ],
+        ]);
+
+        $table->render();
 
         return self::SUCCESS;
     }
