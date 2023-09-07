ef0235fd5eeb82d70a379b1a02e5d948d3b8a8d7
Jérémy Dillenbourg <jeremy@dillenbourg.be>
2023-09-07 13:04:22 +0200

    improve bill recevied

--- module/Billing/Http/Console/BillPaymentReceived.php
+++ module/Billing/Http/Console/BillPaymentReceived.php
@@ -4,10 +4,14 @@ namespace Module\Billing\Http\Console;
 
 use Carbon\CarbonImmutable;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Builder;
 use Module\Billing\Application\GetBillWithPaymentByReferenceQuery;
 use Module\Billing\Application\ReceiveBillPaymentCommand;
+use Module\Billing\Infrastructure\Eloquent\EloquentBill;
 use Module\SharedKernel\Domain\Bus;
 use Module\SharedKernel\Domain\ClockInterface;
+use function Laravel\Prompts\suggest;
+use function Laravel\Prompts\text;
 
 class BillPaymentReceived extends Command
 {
@@ -17,9 +21,9 @@ class BillPaymentReceived extends Command
 
     public function handle(Bus $bus, ClockInterface $clock): int
     {
-        $reference = $this->ask('Enter the bill reference');
-        $amountReceived = $this->ask('Enter the amount received');
-        $receptionDatetime = $this->ask('Enter the date of reception of the amount', $clock->now()->toIso8601String());
+        $reference = suggest('Enter the bill reference', $this->billReferenceWithLeftToPay());
+        $amountReceived = text('Enter the amount received');
+        $receptionDatetime = text('Enter the date of reception of the amount', $clock->now()->toIso8601String());
 
         $command = new ReceiveBillPaymentCommand($reference, $amountReceived, CarbonImmutable::parse($receptionDatetime));
 
@@ -28,5 +32,13 @@ class BillPaymentReceived extends Command
         $this->output->success('Amount credited !');
 
         return self::SUCCESS;
+    }
+
+    private function billReferenceWithLeftToPay(): array
+    {
+        return EloquentBill::query()
+            ->whereDoesntHave('payments')
+            ->pluck('reference')
+            ->toArray();
     }
 }
